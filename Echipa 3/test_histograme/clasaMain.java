7c8dabf83068bc2a78e6c08d379faf5b12ab5de3
ucrisdia <cris.dia.inf@gmail.com>
2015-05-20 09:35:34 +0300

    testJUnitHistograme
    histograme

---
+++ Echipa 3/test_histograme/clasaMain.java
@@ -0,0 +1,35 @@
+﻿public class clasaMain {
+	public static void main(String[] args){
+		
+		/*
+		extractHistogram exH = new extractHistogram("C:\\USERS\\Cris\\Desktop\\","flower2.jpg");
+		
+		int[] rosu = exH.redHistogram();
+		int[] verde = exH.greenHistogram();
+		int[] albastru = exH.blueHistogram();
+		
+
+		System.out.println("\n\n\nROSU:");
+		for(int i=0;i<rosu.length;i++)
+			System.out.print(rosu[i]+",");
+		
+		
+		System.out.println("\n\n\nVERDE:");
+		for(int i=0;i<verde.length;i++)
+			System.out.print(verde[i]+",");
+		
+		System.out.println("\n\n\nALBASTRU:");
+		for(int i=0;i<albastru.length;i++)
+			System.out.print(albastru[i]+",");
+		
+		int medieRosu = exH.redMean();
+		int medieVerde = exH.greenMean();
+		int medieAlbastru = exH.blueMean();
+		
+		System.out.println("\n\nMedie ROSU:"+medieRosu);
+		System.out.println("\n\nMedie VERDE:"+medieVerde);
+		System.out.println("\n\nMedie ALBASTRU:"+medieAlbastru);
+		
+		*/		
+	}
+}
---
+++ Echipa 3/test_histograme/extractHistogram.java
@@ -0,0 +1,77 @@
+﻿import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+
+public class extractHistogram {
+
+	BufferedImage myimg=null;
+	int[] Rfreq, Gfreq, Bfreq;
+	int rgb, width, height;
+	int rcolor, gcolor, bcolor;
+	int sumR, sumG, sumB;
+	
+	extractHistogram(String path, String filename){
+		try {
+			myimg=ImageIO.read(new File(path+filename));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.histograma();
+	}
+	
+	
+	public void histograma(){
+		Rfreq=new int[256];
+        Gfreq=new int[256];
+        Bfreq=new int[256];
+        
+        width = myimg.getWidth();
+        height = myimg.getHeight();
+        
+        for(int i=0;i<width;i++) {
+            for(int j=0;j<height;j++) {
+                 
+                rgb =myimg.getRGB(i,j);
+                rcolor = (rgb >> 16) & 0xff;
+                gcolor = (rgb >>  8) & 0xff;
+                bcolor = (rgb >>  0) & 0xff;
+                Rfreq[rcolor]++;
+                Gfreq[gcolor]++;
+                Bfreq[bcolor]++;
+            }
+        }
+        
+        for(int i=0;i<256;i++){
+        	sumR+=Rfreq[i]*i;
+            sumG+=Gfreq[i]*i;
+            sumB+=Bfreq[i]*i;
+        }
+	}
+	
+	public int[] redHistogram(){
+		return Rfreq;
+	}
+	
+	public int[] greenHistogram(){
+		return Gfreq;
+	}
+	
+	public int[] blueHistogram(){
+		return Bfreq;
+	}
+	
+	public int redMean(){
+		return sumR/(width*height);
+	}
+	
+	public int greenMean(){
+		return sumG/(width*height);
+	}
+	
+	public int blueMean(){
+		return sumB/(width*height);
+	}
+}
