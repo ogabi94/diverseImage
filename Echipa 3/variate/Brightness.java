f7a18a9f077ebaf4d8d1ba093271b716afeb399c
ucrisdia <cris.dia.inf@gmail.com>
2015-05-27 09:23:15 +0300

    variate
    variate

---
+++ Echipa 3/variate/Brightness.java
@@ -0,0 +1,42 @@
+﻿import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Brightness{
+	public static void main(String[] args){
+		Brightness br = new Brightness();
+		try {
+			ImageIO.write(br.brightnessModify(ImageIO.read(new File("C:\\USERS\\Cris\\Desktop\\cflower2.jpg")), -180),"jpg",new File("C:\\USERS\\Cris\\Desktop\\BRcflower2.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+    public BufferedImage brightnessModify(BufferedImage input, int factor) {
+        int w = input.getWidth();
+        int h = input.getHeight();
+        BufferedImage output = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                Color clr = new Color(input.getRGB(i, j));
+                int r, g, b;
+                r = clr.getRed() + factor;
+                g = clr.getGreen() + factor;
+                b = clr.getBlue() + factor;
+                if(r >= 256) {r = 255;
+                } else if (r < 0) {
+                    r = 0;}
+                if (g >= 256) {g = 255;
+                } else if (g < 0) {
+                    g = 0;}
+                if (b >= 256) {b = 255;
+                } else if (b < 0) {
+                    b = 0;}
+                output.setRGB(i, j, new Color(r, g, b).getRGB());
+            }
+        }
+        return output;
+    }
+}
---
+++ Echipa 3/variate/CompareWITHPixelGrabber.java
@@ -0,0 +1,41 @@
+﻿import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.PixelGrabber;
+ 
+public class CompareWITHPixelGrabber {
+	static void procesare() {		
+		String file1 = "C:\\USERS\\Cris\\Desktop\\Lenna100.jpg";
+		String file2 = "C:\\USERS\\Cris\\Desktop\\Lenna50.jpg";
+ 
+		Image img1 = Toolkit.getDefaultToolkit().getImage(file1);
+		Image img2 = Toolkit.getDefaultToolkit().getImage(file2);
+ 
+		try {
+			PixelGrabber g1 =new PixelGrabber(img1,0,0,-1,-1,false);
+			PixelGrabber g2 =new PixelGrabber(img2,0,0,-1,-1,false);
+			int[] rez1 = null;
+			int[] rez2 = null;
+			if (g1.grabPixels()) {
+				int w = g1.getWidth();
+				int h = g1.getHeight();
+				rez1 = new int[w*h];
+				rez1 = (int[]) g1.getPixels();
+			}
+			if (g2.grabPixels()) {
+				int w = g1.getWidth();
+				int h = g2.getHeight();
+				rez2 = new int[w*h];
+				rez2 = (int[]) g2.getPixels();
+			}
+			if(java.util.Arrays.equals(rez1, rez2))
+				System.out.println("Imaginile sunt identice.");
+			else
+				System.out.println("Imaginile sunt diferite.");
+		} catch (InterruptedException excep) {
+			excep.printStackTrace();
+		}
+	}
+	public static void main(String args[]) {
+		procesare();
+	}
+}
